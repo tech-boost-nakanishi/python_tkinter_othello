3ffb4ee9320e19a538207f269aa27e515bff3dfa
中西　洋翔 <aibou@nakanishiyoushounoiMac.local>
2021-07-27 16:45:46 +0900

    ボードクラス追加

---
+++ board.py
@@ -0,0 +1,32 @@
+from decimal import Decimal, ROUND_HALF_UP
+
+class Board():
+	def __init__(self, canvas, blength, gfwidth):
+		self.canvas = canvas
+		self.blength = blength
+		self.gfwidth = gfwidth
+		self.masuwidth = self.masuheight = int(Decimal(str(self.gfwidth / self.blength)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+
+	def getX(self, mx):
+		for i in range(self.blength):
+			if mx >= i * self.masuwidth and mx <= i * self.masuwidth + self.masuwidth:
+				return i
+
+	def getY(self, my):
+		for i in range(self.blength):
+			if my >= i * self.masuheight and my <= i * self.masuheight + self.masuheight:
+				return i
+
+	def drawBoard(self):
+		# ボード背景
+		self.canvas.create_rectangle(0, 0, self.gfwidth, self.gfwidth, fill = 'darkgreen')
+
+		## 境界線
+		linecolor = 'black'
+		# 横線
+		for i in range(self.blength):
+			self.canvas.create_line(0, (i + 1) * self.masuheight, self.gfwidth, (i + 1) * self.masuheight, fill = linecolor, width = 2)
+
+		# 縦線
+		for i in range(self.blength):
+			self.canvas.create_line((i + 1) * self.masuwidth, 0, (i + 1) * self.masuwidth, self.gfwidth, fill = linecolor, width = 2)
--- gameFrame.py
+++ gameFrame.py
@@ -5,14 +5,32 @@ class GameFrame(tk.Frame):
 
 	def __init__(self, parent, controller):
 		self.WIDTH = 640
-		self.HEIGHT = 750
+		self.HEIGHT = 720
 		tk.Frame.__init__(self, parent, width = self.WIDTH, height = self.HEIGHT)
 		self.pack_propagate(0)
+		self.bgcolor = 'lightgray'
+		self.masucount = 8
 
-		tk.Label(self, text = 'ゲームフレーム').pack()
+		self.canvas = tk.Canvas(self, width = self.WIDTH, height = self.HEIGHT, bg = self.bgcolor)
+		self.canvas.pack()
+
+		# ボードクラスのインスタンス生成
+		import board
+		self.boardobj = board.Board(self.canvas, self.masucount, self.WIDTH)
+
+		tk.Button(self, text = '最初から', bg = self.bgcolor, highlightbackground = self.bgcolor, width = 10, command = lambda: main.show_frame('ゲームフレーム')).place(x = 10, y = 650)
+		tk.Button(self, text = '終了', bg = self.bgcolor, highlightbackground = self.bgcolor, width = 10, command = controller.destroy).place(x = 10, y = 680)
+		tk.Button(self, text = 'メニュー画面へ', bg = self.bgcolor, highlightbackground = self.bgcolor, width = 10, command = lambda: main.show_frame('メニューフレーム')).place(x = 120, y = 650)
+		tk.Button(self, text = '設定画面へ', bg = self.bgcolor, highlightbackground = self.bgcolor, width = 10, command = lambda: main.show_frame('設定フレーム')).place(x = 120, y = 680)
+
+		self.paint()
 
 	def getWidth(self):
 		return self.WIDTH
 
 	def getHeight(self):
 		return self.HEIGHT
+
+	def paint(self):
+		# ボードを描画
+		self.boardobj.drawBoard()
